744746ac23f218a5ae0a36969b18c4ff2fef6865
Kevin Yoon <kevin.michael.yoon@gmail.com>
2020-06-19 20:11:23 -0400

    DDL and Seed scripts

---
+++ Database/scripts/Seed.sql
@@ -0,0 +1,22 @@
+/*
+	SEED Script Initial Categories
+*/
+DELETE FROM Categories;
+
+INSERT INTO Categories (CategoryName) VALUES 
+('Rent'),
+('Student Loans'),
+('Installment Loan'),
+('Car Insurance'),
+('Electricity'),
+('Internet'),
+('Emergency Savings'),
+('Long Term Savings'),
+('Short Term Savings'),
+('Traditional IRA'),
+('Cash'),
+('Renters Insurance'),
+('Spotify'),
+('Netflix'),
+('AMC A List'),
+('Credit Card Payments');
